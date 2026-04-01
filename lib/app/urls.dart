c052c8fa370398ad66d5be4831e11816bf38ab26
tanviremon001 <tanvirofficial121@gmail.com>
2026-04-01 23:03:23 +0600

    refactor: folder structure and necessary dart file created

---
+++ lib/app/urls.dart
@@ -0,0 +1,6 @@
+class Urls {
+  static const String _baseAssetPath = "assets/images";
+
+  static const String blackAppLogo = "$_baseAssetPath/black_app_logo_1.svg";
+  static const String whiteAppLogo = "$_baseAssetPath/white_app_logo_1.svg";
+}
