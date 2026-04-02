7c27070956c7876b4d5987d36a2a2e96b568c88f
tanviremon001 <tanvirofficial121@gmail.com>
2026-04-02 17:07:42 +0600

    chore: native_splash_screen package configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shopping_app/app/shopping_app.dart';
 
 void main(){
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(ShoppingApp());
 }
