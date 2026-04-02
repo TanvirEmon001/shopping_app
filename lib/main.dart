b175730a6407e5078d7a47f6731a72fdf956addc
tanviremon001 <tanvirofficial121@gmail.com>
2026-04-02 17:41:37 +0600

    fix: some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shopping_app/app/shopping_app.dart';
 
 void main(){
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(ShoppingApp());
 }
