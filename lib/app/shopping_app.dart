e6aa97f5a6804726e908efca26186f63491d98d1
tanviremon001 <tanvirofficial121@gmail.com>
2026-04-02 18:34:31 +0600

    fix: some fixes

--- lib/app/shopping_app.dart
+++ lib/app/shopping_app.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:shopping_app/app/routers/routers.dart';
-import 'package:shopping_app/features/auth/presentation/screens/sign_in_screen.dart';
-import 'package:shopping_app/features/splash/presentation/screens/splash_screen.dart';
+
 
 
 class ShoppingApp extends StatelessWidget {
-  ShoppingApp({super.key});
+  const ShoppingApp({super.key});
 
   @override
   Widget build(context){
@@ -15,10 +13,6 @@ class ShoppingApp extends StatelessWidget {
     );
   }
 
-  final GoRouter _router = GoRouter(routes: [
-    GoRoute(path: "/", builder: (context, state) => const SignInScreen()),
-    GoRoute(path: "/splash", builder: (context, state) => const SplashScreen())
-  ]);
 
 }
 
