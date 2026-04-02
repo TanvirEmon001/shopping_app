d2bace8c1e34f6748d73c4e5b703800bd994c97f
tanviremon001 <tanvirofficial121@gmail.com>
2026-04-02 19:48:29 +0600

    design: sign in screen design in progress

--- lib/app/app_theme.dart
+++ lib/app/app_theme.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final ThemeData _lightThemeData = ThemeData(
     brightness: .light,
-    scaffoldBackgroundColor: Colors.white
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TextTheme(
+      bodySmall: TextStyle(color: Colors.black),
+      displaySmall: TextStyle(color: Colors.black),
+    ),
+
   );
 
   static final ThemeData _darkThemeData = ThemeData(
-      brightness: .dark,
-      scaffoldBackgroundColor: Colors.black
+    brightness: .dark,
+    scaffoldBackgroundColor: Colors.black,
   );
 
   static ThemeData get lightTheme => _lightThemeData;
